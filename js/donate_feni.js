01e8640dbc47d25fc902ab94a1b2a5ebd2f8fd88
saumittras <saumittra.eee@gmail.com>
2024-09-23 21:30:28 +0600

    added all function and added feni section

--- js/donate_feni.js
+++ js/donate_feni.js
@@ -1,1 +1,31 @@
-document.getElementById();
+document.getElementById("donateFeni").addEventListener("click", function () {
+  // getting input value by id
+  const feniDonateAmount = getInputValuById("feniDonateInput");
+  console.log(feniDonateAmount);
+
+  // input validation amount validationInput(parseAmount)
+  validationInput(feniDonateAmount);
+
+  // feni current blance
+  const feniBlance = getInnerTextById("feni-amount");
+  console.log(feniBlance);
+
+  //Get Main Blance Amount
+  const mainBlance = getInnerTextById("main-blance");
+  console.log(mainBlance);
+
+  // Blance validation
+  donateBlanceValidation(mainBlance, feniDonateAmount);
+
+  // added the donation amount to the noyakhali Blance
+  addDonationAmount("feni-amount", feniBlance, feniDonateAmount);
+
+  //reduce the donate amount from main Balance
+  reduceDonationAmount(mainBlance, feniDonateAmount);
+
+  // Added transetion conformation modal
+  my_modal_5.showModal();
+
+  // added history
+  addDonateHistory(feniDonateAmount, "Relief in Feni");
+});
--- js/utility.js
+++ js/utility.js
@@ -4,6 +4,39 @@ function getInputValuById(id) {
   return value;
 }
 
+// Input validation for donate amount input
+function validationInput(parseAmount) {
+  if (isNaN(parseAmount) || parseAmount <= 0) {
+    return alert("⚠️Ivalid Donate Amount⚠️. Please Enter a Valid Input Amount");
+  }
+}
+
+// get innerText by id
+function getInnerTextById(id) {
+  const innerTextValue = parseFloat(document.getElementById(id).innerText);
+  return innerTextValue;
+}
+// Blance validation
+function donateBlanceValidation(mainBlance, donateBalance) {
+  if (mainBlance < donateBalance) {
+    return alert("Insufflation Balance");
+  }
+}
+
+// added the donation amount to the noyakhali Blance
+function addDonationAmount(sectionId, sectionBalance, donateAmount) {
+  document.getElementById(sectionId).innerText = (
+    sectionBalance + donateAmount
+  ).toFixed(2);
+}
+
+//reduce the donate amount from main Balance
+function reduceDonationAmount(mainBlances, donateAmount) {
+  document.getElementById("main-blance").innerText = (
+    mainBlances - donateAmount
+  ).toFixed(2);
+}
+
 // remove class name by id and ClassName
 function removeClassNameById(id) {
   const ccc = document.getElementById(id);
@@ -25,7 +58,8 @@ function addDonateHistory(donateBlance, place) {
     "rounded-lg",
     "bg-white",
     "border-gray-300",
-    "py-8"
+    "py-8",
+    "mb-6"
   );
   const newDateTime = new Date();
   newDateTime.setFullYear(2024);
@@ -40,5 +74,5 @@ function addDonateHistory(donateBlance, place) {
     `;
   const historySection = document.getElementById("history");
 
-  historySection.insertBefore(donateHistory, historyContainer.firstChild);
+  historySection.insertBefore(donateHistory, historySection.firstChild);
 }
